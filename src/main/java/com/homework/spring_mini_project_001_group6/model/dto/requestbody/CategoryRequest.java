76ada6dae6c822e982dc763da06133aac98e014c
SUnMArI <thy.sopheak098@gmail.com>
2024-08-26 13:44:27 +0700

    change validation

--- src/main/java/com/homework/spring_mini_project_001_group6/model/dto/requestbody/CategoryRequest.java
+++ src/main/java/com/homework/spring_mini_project_001_group6/model/dto/requestbody/CategoryRequest.java
@@ -1,5 +1,7 @@
 package com.homework.spring_mini_project_001_group6.model.dto.requestbody;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,5 +12,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CategoryRequest {
+    @NotBlank(message = "Category name can't be blank")
+    @Pattern(regexp = "^[a-zA-Z0-9 ]+$", message = "Name can not contain any special characters")
     private String categoryName;
 }
--- src/main/java/com/homework/spring_mini_project_001_group6/service/serviceimp/CategoryServiceImp.java
+++ src/main/java/com/homework/spring_mini_project_001_group6/service/serviceimp/CategoryServiceImp.java
@@ -1,6 +1,8 @@
 package com.homework.spring_mini_project_001_group6.service.serviceimp;
 
+import com.homework.spring_mini_project_001_group6.exception.ConflictException;
 import com.homework.spring_mini_project_001_group6.exception.InvalidDataException;
+import com.homework.spring_mini_project_001_group6.exception.SearchNotFoundException;
 import com.homework.spring_mini_project_001_group6.model.dto.requestbody.CategoryRequest;
 import com.homework.spring_mini_project_001_group6.model.dto.response.ApiResponse;
 import com.homework.spring_mini_project_001_group6.model.dto.response.CategoryResponse;
@@ -10,23 +12,23 @@ import com.homework.spring_mini_project_001_group6.repository.CategoryRepository
 import com.homework.spring_mini_project_001_group6.repository.UserRepository;
 import com.homework.spring_mini_project_001_group6.service.CategoryService;
 import com.homework.spring_mini_project_001_group6.util.SortByCategoryField;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class CategoryServiceImp implements CategoryService {
 
-    @Autowired
+
     private CategoryRepository categoryRepository;
 
-    @Autowired
+
     private UserRepository userRepository;
 
    @Override
@@ -35,7 +37,7 @@ public class CategoryServiceImp implements CategoryService {
 
         categoryRepository.findByCategoryNameAndUser_UserId(categoryRequest.getCategoryName(), userId)
                 .ifPresent(category -> {
-                    throw new InvalidDataException("Duplicate category name: " + categoryRequest.getCategoryName());
+                    throw new ConflictException("Duplicate category name: " + categoryRequest.getCategoryName());
                 });
 
         Category category = new Category();
@@ -59,7 +61,10 @@ public class CategoryServiceImp implements CategoryService {
        if(sortDirection.compareToIgnoreCase("asc") !=0 & sortDirection.compareToIgnoreCase("desc") !=0) {
            throw new InvalidDataException("Invalid sort direction");
        }
-        List<Category> categories = categoryRepository.findAllByUser_userId(PageRequest.of(pageNo,pageSize, Sort.Direction.valueOf(sortDirection.toUpperCase()), String.valueOf(sortBy)),userId);
+        List<Category> categories = categoryRepository.findAllByUser_userId(
+                PageRequest.of(pageNo,pageSize,
+                        Sort.Direction.valueOf(sortDirection.toUpperCase()),
+                        String.valueOf(sortBy)),userId);
         List<CategoryResponse> categoryResponses = categories.stream().map(Category::toCategoryResponse).toList();
         return  new ApiResponse<>("Get all category successfully.", HttpStatus.OK, categoryResponses);
     }
@@ -70,22 +75,22 @@ public class CategoryServiceImp implements CategoryService {
         if(categoryOptional.isPresent()){
             Category category = categoryOptional.get();
             return new ApiResponse<>("Get category with id " + id + " successfully", HttpStatus.OK, Category.toCategoryResponse(category));
-        };
-        throw new InvalidDataException("Category not found");
+        }
+        throw new SearchNotFoundException("Category with id "+id+" is not found");
     }
 
     @Override
     public ApiResponse<CategoryResponse> updateCategoryById(Long id, CategoryRequest categoryRequest, Long userId) {
               categoryRepository.findByCategoryNameAndUser_UserId(categoryRequest.getCategoryName(), userId)
                 .ifPresent(category -> {
-                    throw new InvalidDataException("Duplicate category name: " + categoryRequest.getCategoryName());
+                    throw new ConflictException("Duplicate category name: " + categoryRequest.getCategoryName());
                 });
              Category category = categoryRepository
                      .findByCategoryIdAndUser_userId(id, userId)
                      .stream()
                      .filter(e-> e.getCategoryId()!=null).findFirst()
                      .map(category1 -> { category1.setCategoryName(categoryRequest.getCategoryName()); return categoryRepository.save(category1); } )
-                     .orElseThrow(()-> new InvalidDataException("Category not found"));
+                     .orElseThrow(()-> new SearchNotFoundException("Category with id "+id+" is not found"));
              return new ApiResponse<>("Update successfully",HttpStatus.OK,Category.toCategoryResponse(category));
     }
 
@@ -94,9 +99,8 @@ public class CategoryServiceImp implements CategoryService {
         categoryRepository.findByCategoryIdAndUser_userId(id, userId).stream()
                 .filter(e -> e.getCategoryId()!=null).findFirst()
                 .map(category -> { categoryRepository.delete(category); return Category.toCategoryResponse(category); } )
-                .orElseThrow(()-> new InvalidDataException("Category not found"));
+                .orElseThrow(()-> new SearchNotFoundException("Category with id "+id+" is not found"));
         return new ApiResponse<>("Category with id "+id+" is deleted",HttpStatus.OK,null);
     }
-
 
 }
